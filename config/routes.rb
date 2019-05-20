0548ded25248bb6e3cc1cec77fe592e34dbe6e32
MargaritaKhodzka <mkhodzka@gmail.com>
2019-05-19 22:43:00 -0400

    added show action to tags controller, created destinations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'destinations/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tags' => 'tags#index'
 end
